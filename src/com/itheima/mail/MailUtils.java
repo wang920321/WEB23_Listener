1e4de30e71a4a0468e1d7c90fd41d0bec331de41
wang920321 <m13372782171_1@163.com>
2018-03-09 09:16:02 +0800

    邮件发送

---
+++ src/com/itheima/mail/MailUtils.java
@@ -0,0 +1,60 @@
+package com.itheima.mail;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
+public class MailUtils {
+
+	/**
+	 * @param email 邮件发给谁？
+	 * @param subject 代表主题
+	 * @param emailMsg  邮件内容
+	 * @throws AddressException
+	 * @throws MessagingException
+	 */
+	public static void sendMail(String email,String subject, String emailMsg)
+			throws AddressException, MessagingException {
+		
+		// 1.创建一个程序与邮件服务器会话对象 Session
+
+		Properties props = new Properties();
+		props.setProperty("mail.transport.protocol", "SMTP");//发邮件的协议
+		props.setProperty("mail.host", "smtp.163.com");//发邮件的服务器地址
+		props.setProperty("mail.smtp.auth", "true");// 指定验证为true
+
+		// 创建验证器
+		Authenticator auth = new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("m13372782171_1", "qq920321..");//发邮件的账号的验证
+			}
+		};
+
+		Session session = Session.getInstance(props, auth);
+
+		// 2.创建一个Message，它相当于是邮件内容
+		Message message = new MimeMessage(session);
+
+		message.setFrom(new InternetAddress("m13372782171_1@163.com")); // 设置发送者
+
+		message.setRecipient(RecipientType.TO, new InternetAddress(email)); // 设置发送方式与接收者
+
+		message.setSubject(subject);//设置邮件的主题
+		// message.setText("这是一封激活邮件，请<a href='#'>点击</a>");
+
+		message.setContent(emailMsg, "text/html;charset=utf-8");
+
+		// 3.创建 Transport用于将邮件发送
+
+		Transport.send(message);
+	}
+}
---
+++ src/com/itheima/mail/SendMailTest.java
@@ -0,0 +1,13 @@
+package com.itheima.mail;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+public class SendMailTest {
+
+	public static void main(String[] args) throws AddressException, MessagingException {
+		MailUtils.sendMail("172219902@qq.com","测试邮件", "这是一封测试邮件2");
+
+	}
+
+}
